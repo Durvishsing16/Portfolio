64c59c7815020157b0aef4af6050ff1f828aa7f5
Durvishsing16 <durvishsingbawanypeck@gmail.com>
2024-10-08 22:32:38 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,57 @@
+window.PorfolioConfig = {
+  name: 'Durvishsing Bawanypeck',
+  jobTitle: '2nd Year Cybersecurity Student',
+  jobDescription: "Welcome to my portfolio! I'm Durvishsing Bawanypeck, a passionate cybersecurity student and web developer. Explore my work, skills, and projects as I continue my journey in technology and cybersecurity. Feel free to reach out!",
+  skills: [
+    {
+      name: 'Python',
+      description: '2 year experience in Python'
+    },
+    {
+      name: 'HTML',
+      description: '2 Year experience'
+    },
+    {
+      name: 'CSS',
+      description: '2 years of experience'
+    }
+  ],
+  works: [
+    {
+      name: 'Map marker',
+      description: 'This project developped during my internship at Blink by Emtel allows you to have a map on a website ,through the use of Google Maps API, HTML, CSS and JS,to place markers on the map by using their coordinates. The markers can also be filtered through different categories.'
+    },
+    {
+      name: 'Vending Machine',
+      description: 'My vending machine project is one made for my final coursework at the end of my first year.It was made entirely using Python and the Tkinter library. It uses a GUI to simulate the process of buying drinks from a machine. From choice input to the checkout, everything is covered.'
+    },
+    {
+      name: 'Financial tracker',
+      description: 'This is an upcoming project of mine using Python whereby the programwill allow you to track your expenses and savings in an excel sheet. Additionally the programwill offer the user to consult a financial advisor bot through the use of Open AI.'
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Coding',
+      description: 'In the field of IT, if you do not keep in touchwith practice, you will fall behond. It is also importantto have passion in what you do.'
+    },
+    {
+      name: 'Console Gaming',
+      description: 'As a person who grew up with consoles, it is a hobby that has never gone away.It is as important to let off some steam and disconnect from the outside worldonce in a while.'
+    },
+    {
+      name: 'Reading',
+      description: 'Unfortunately reading has not been the hobby I partake in the mostbut a good sci-fi or fantasy book is always enjoyable once in a while.'
+    }
+  ],
+  contacts: {
+    email: 'durvishsingbawanypeck@gmail.com',
+    mobileNo: '59732880',
+    links: [
+      {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/durvishsing-bawanypeck-88aa42258/'
+      }
+    ]
+  }
+};
